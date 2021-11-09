6b5f4407478c4bbad899e9079d086761be41fbc5
signess <iky_2000@hotmail.com>
2021-11-09 17:08:35 -0400

    switch scene 2

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
+    [SerializeField] private int sceneToLoad = -1;
+    [SerializeField] private Transform spawnPoint;
+
+    private PlayerController player;
+
+    public Transform SpawnPoint => spawnPoint;
+
     public void OnPlayerTrigger(PlayerController player)
     {
-        print("Step on portal");
+        this.player = player;
+        StartCoroutine(SwitchScene());
+    }
+
+    private IEnumerator SwitchScene()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+        var destPortal = FindObjectsOfType<Portal>().First(x => x != this);
+        player.transform.position = destPortal.SpawnPoint.position;
+        Destroy(gameObject);
     }
 
 }
